6b3640d9454493856a0b0014dabc7b145325f9e2
Konstantin Divilov <30414893+kdivilov@users.noreply.github.com>
2018-02-06 10:10:51 -0500

    Update rnaseqpipeline.sh

--- RNA-Seq/rnaseqpipeline.sh
+++ RNA-Seq/rnaseqpipeline.sh
@@ -1,3 +1,5 @@
+#RNA-Seq fastq files: https://www.ncbi.nlm.nih.gov/bioproject/281110
+
 #HISAT2: https://ccb.jhu.edu/software/hisat2/index.shtml (Note that a newer version has been released and the code below should be changed to reflect this if using the newer version)
 #StringTie: https://ccb.jhu.edu/software/stringtie/
 #Ballgown: https://github.com/alyssafrazee/ballgown
@@ -29,7 +31,7 @@ done
 
 stringtie-1.3.3b.Linux_x86_64/stringtie --merge -p 8 -G ref/all_V1_NR_mrna_new_posit_on_assembly_v2_without_chrUkn.gff3 -o  stringtieoutput/allmerged.gtf mergelist.txt
 
-#Ballgown
+#Setup for Ballgown analysis
 
 for filename in stringtieoutput/*.gtf; do
     echo "$filename"
